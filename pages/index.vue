5be5de36633cf4752d61ffdaed1365ec3939dcac
ecem <118804065+EcemAydn@users.noreply.github.com>
2023-08-11 11:15:58 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,153 +1,185 @@
 <script setup>
-import LayerTwoIcon from '@/components/icons/LayerTwoIcon.vue';
-import { useAuthStore } from '@/stores/auth/auth';
-import SpaceShip from '@/components/icons/SpaceShip.vue';
+import themeConfig from '@/themeConfig';
 
-const authStore = useAuthStore();
-
-const headers = ref([
-  { name: "NAME", value: "name" },
-  { name: "POSITION", value: "position" },
-  { name: "COUNTRY", value: "country" },
-  { name: "STATUS", value: "status" },
-  { name: "EMAIL", value: "email" },
-]);
-
-const users = ref([
-  {
-    id: 1,
-    avatar: 'https://i.pravatar.cc',
-    name: "Ecem Beyza Aydın",
-    email: "ecemaydn15@gmail.com",
-    position: "Senior Frontend Developer",
-    country: "İtalya",
-    status: "Active",
-  },
-  {
-    id: 2,
-    avatar: "https://i.pravatar.cc",
-    name: "Ceren Ozkurt",
-    email: "cerenozkurt@gmail.com",
-    position: "Senior Backend Developer",
-    country: "Arjantin",
-    status: "Active",
-  },
-  {
-    id: 3,
-    avatar: "https://i.pravatar.cc",
-    name: "Tirami Su",
-    email: "tiramisu@gmail.com",
-    position: "Senior Frontend Developer",
-    country: "Turkey",
-    status: "Active",
-  },
-  {
-    id: 4,
-    avatar: null,
-    name: "Arda Turan",
-    email: "ardaturan@gmail.com",
-    position: "Futbolcu",
-    country: "Turkey",
-    status: "Active",
-  },
-  {
-    id: 5,
-    avatar: null,
-    name: "Fettucini Gulen",
-    email: "fettucini@gmail.com",
-    position: "Bölücü",
-    country: "Amerika",
-    status: "Active",
-  },
-]);
-
-// function test() {
-//   fetch('/api/hello').then((response) => response.json()).then((data) => {
-//     console.log(data);
-//   })
-// }
+import ChevronDownSmall from '@/components/icons/ChevronDownSmall.vue';
+import ChevronRightSmall from '@/components/icons/ChevronRightSmall.vue';
+import UploadIcon from '@/components/icons/UploadIcon.vue';
+import RefreshIcon from '@/components/icons/RefreshIcon.vue';
+import LockIcon from '@/components/icons/LockIcon.vue';
+import TouchIdIcon from '@/components/icons/TouchIdIcon.vue';
 
 definePageMeta({
-  // middleware: ['route-middleware'],
-})
+  layout: "blank",
+});
 </script>
-
 <template>
-  <div class="flex flex-col gap-6">
-    <div class="text-2xl">
-      <div class="flex items-center gap-2">
-          <IconBase class="w-6 h-6 sm:!w-10 sm:!h-10" viewBox="0 0 512 512" color="blue">
-            <SpaceShip />
-          </IconBase>
-          <div class="text-xl sm:!text-3xl">
-            Hey {{ authStore.currentUser.givenName.toUpperCase() }}!
+  <main class="w-full h-full overflow-y-auto">
+    <header class="flex h-full w-full overflow-y-auto">
+      <nav class="flex flex-col w-full bg-[#f6f7f9] h-full">
+        <div
+          class="flex items-center gap-4 py-1 w-full justify-between text-content-primary font-medium sm:px-8"
+        >
+          <img :src="themeConfig.logo" alt="logo" class="w-12 h-12">
+          <NuxtLink to="/home">
+              Home
+            </NuxtLink>
+          <!-- <div class="items-center gap-8 hidden sm:flex">
+            <NuxtLink to="/home">
+              Home
+            </NuxtLink>
+            <NuxtLink to="/home" class="pl-6">
+              Components
+            </NuxtLink>
+            <div>
+              <DropdownComponent>
+                <template #activator>
+                  <div class="flex font-medium items-center text-content-primary">
+                    Examples
+                    <IconBase>
+                      <ChevronDownSmall />
+                    </IconBase>
+                  </div>
+                </template>
+
+                <DropdownItemComponent v-for="(item, index) in 2" :key="index" >
+                  <div class="flex items-center gap-2 whitespace-nowrap">
+                    Forms
+                  </div>
+                </DropdownItemComponent>
+              </DropdownComponent>
+            </div>
+          </div> -->
+          <NuxtLink to="/auth/register">
+            <ButtonComponent variant="ghost" appearance="secondary">
+              Get Started
+              <template #append>
+                <IconBase>
+                  <ChevronRightSmall />
+                </IconBase>
+              </template>
+            </ButtonComponent>
+          </NuxtLink>
+        </div>
+        <div class="flex flex-col gap-6 items-center justify-center h-full relative">
+          <div aria-hidden="true" class=" absolute top-40 left-0 lg:left-80 w-60 h-60 bg-background-brand blur-lg opacity-30 rounded-full"></div>
+          <div aria-hidden="true" class="absolute bottom-40 right-0 lg:right-80 w-60 h-60 bg-background-brand blur-lg opacity-30 rounded-full"></div>
+          <NuxtLink to="/auth/register">
+            <ButtonComponent appearance="secondary" size="small" class="bg-transparent">
+              <div class="flex flex-col sm:flex-row gap-2 items-center">
+                Ocean Starter Kit is now available for Nuxt 3! 
+                <div class="flex items-center text-content-informative">
+                  Read more 
+                  <IconBase>
+                    <ChevronRightSmall />
+                  </IconBase>
+                </div>
+              </div>
+            </ButtonComponent>
+          </NuxtLink>
+          <h1 class="text-4xl font-bold">OCEAN STARTER KIT</h1>
+          <p class="text-content-secondary text-center">Beautifully designed, fully featured, and ready to go starter kit for Nuxt 3 and<br>Tailwind CSS.</p>
+          <div class="flex items-center gap-4">
+            <NuxtLink to="/auth/register">
+              <ButtonComponent>Get Started</ButtonComponent>
+            </NuxtLink>
+            <a href="https://github.com/Ron-Digital/ocean-starter-kit">
+              <ButtonComponent variant="ghost">View Github
+                <IconBase>
+                  <ChevronRightSmall />
+                </IconBase>
+              </ButtonComponent>
+            </a>
           </div>
         </div>
-    </div>
-    <div class="flex flex-col sm:flex-row gap-4">
-      <!-- <ButtonComponent text="deneme" @click="test" /> -->
-      <CardComponent size="medium" line>
-        <template #title>
-          Example Card
-        </template>
-        <div class="p-6 text-sm font-light">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br>
-          Suscipit officiis vero, ducimus aperiam repudiandae fuga consequatur recusandae assumenda sequi aspernatur aut, asperiores blanditiis impedit!<br><br>
-          <div class="flex gap-2 items-center mb-4"> 
-            <i> Repellendus iste quod odit sunt? Debitis numquam modi mollitia veniam dolore aperiam nobis, accusantium a perspiciatis.</i>
-            <img src="https://i.pravatar.cc" alt="Avatar" class="rounded-xl w-28 opacity-90" />
-          </div>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br>
-          Suscipit officiis vero, ducimus aperiam repudiandae fuga consequatur recusandae assumenda sequi aspernatur aut, asperiores blanditiis impedit!<br><br>
+      </nav>
+    </header>
+
+    <section class="bg-[#f6f7f9] flex flex-col lg:flex-row justify-between w-full gap-8 relative">
+      <aside class="md:pl-32 px-4 md:pr-20 py-8 flex flex-col gap-4">
+        <div class="text-3xl font-bold">Everything you need to create your app</div>
+        <div class="text-content-secondary font-medium">
+          Includes everything you need to build your app, including a customizable UI kit, authentication, and more.
         </div>
-        <template #prepend>
-          <AvatarComp color="blue" radius="full">
+        <div class="text-content-secondary flex items-start text-sm gap-2 py-4">
+          <AvatarComp color="blue" class="pt-1">
             <template #icon>
-              <IconBase>
-                <LayerTwoIcon />
+              <IconBase viewBox="0 0 20 22">
+                <UploadIcon />
               </IconBase>
             </template>
           </AvatarComp>
-        </template>
-      </CardComponent>
-      <CardComponent size="full" line>
-        <template #title>
-          {{ $t('auth.LoginHeader')}}
-        </template>
-        <div class="p-6 text-sm font-light">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br>
-          Suscipit officiis vero, ducimus aperiam repudiandae fuga consequatur recusandae assumenda sequi aspernatur aut, asperiores blanditiis impedit!<br><br>
-          <div class="flex gap-2 items-center mb-4"> 
-            <i> Repellendus iste quod odit sunt? Debitis numquam modi mollitia veniam dolore aperiam nobis, accusantium a perspiciatis.</i>
-            <img src="https://i.pravatar.cc" alt="Avatar" class="rounded-xl w-28 opacity-90" />
-          </div>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br>
-          Suscipit officiis vero, ducimus aperiam repudiandae fuga consequatur recusandae assumenda sequi aspernatur aut, asperiores blanditiis impedit!<br><br>
-        </div>
-        <template #prepend>
-          <AvatarComp color="blue" radius="full">
-            <template #icon>
-              <IconBase>
-                <LayerTwoIcon />
-              </IconBase>
-            </template>
-          </AvatarComp>
-        </template>
-      </CardComponent>
-    </div>
-    <div class="text-xl mt-8">
-      Thank you!
-    </div>
-    <TableComponent :headers="headers" :items="users">
-      <template #name="{ item }">
-        <div class="flex items-center gap-2">
-          <AvatarComp radius="full" size="small" :src="item.avatar" :avatar-text="item.name" />
           <div>
-            <p class="">{{ item.name }}</p>
-            {{ item.email }}
+            <b>Nuxt 3</b> The latest and greatest version of Nuxt.js, with a new file system based routing system and a new way to write your components.
           </div>
         </div>
-      </template>
-    </TableComponent>
-  </div>
+        <div class="text-content-secondary flex items-start text-sm gap-2 py-4">
+          <AvatarComp color="blue" class="pt-1">
+            <template #icon>
+              <IconBase viewBox="0 0 16 20">
+                <LockIcon />
+              </IconBase>
+            </template>
+          </AvatarComp>
+          <div>
+            <b>Tailwind CSS</b> A utility-first CSS framework packed with classes that can be composed to build any design, directly in your markup.
+          </div>
+        </div>
+        <div class="text-content-secondary flex items-start text-sm gap-2 py-4">
+          <AvatarComp color="blue" class="pt-1">
+            <template #icon>
+              <IconBase viewBox="0 0 14 20">
+                <RefreshIcon />
+              </IconBase>
+            </template>
+          </AvatarComp>
+          <div>
+            <b>Ronded UI</b> Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind CSS.
+          </div>
+        </div>
+        <div class="text-content-secondary flex items-start text-sm gap-2 py-4">
+          <AvatarComp color="blue" class="pt-1">
+            <template #icon>
+              <IconBase viewBox="0 0 24 24">
+                <TouchIdIcon />
+              </IconBase>
+            </template>
+          </AvatarComp>
+          <div>
+            <b>Built-in Components</b> A collection of UI components built with Tailwind CSS and Ronded UI, ready to be used in your projects.
+          </div>
+        </div>
+      </aside>
+      <figure class="flex flex-col items-center gap-4 w-full">
+        <img src="../assets/img/feature-1.png" class="lg:absolute right-0 -bottom-20 w-[500px] rounded-xl" alt="Description of the Image">
+        <img src="../assets/img/feature-2.png" class="w-[600px] lg:w-[800px] rounded-xl" alt="Description of the Image">
+      </figure>
+    </section>
+    <footer class="bg-[#f6f7f9] pt-40">
+      <div class="flex items-center justify-center">
+        <div class="bg-gradient-to-r from-background-informative px-4 md:px-0 to-[#f6f7f9] w-full py-20 items-center flex flex-col md:flex-row gap-8 justify-between">
+          <div class="flex flex-col gap-6 px-20 text-white">
+            <div class="font-bold text-3xl">Boost your productivity</div>
+            <div class="">Start building your next app with Ocean Starter Kit today.</div>
+            <div class="flex items-center gap-4">
+              <NuxtLink to="/auth/register">
+                <ButtonComponent>Get Started</ButtonComponent>
+              </NuxtLink>
+              <a href="https://github.com/Ron-Digital/ocean-starter-kit">
+              <ButtonComponent>View Github
+                <IconBase>
+                  <ChevronRightSmall />
+                </IconBase>
+              </ButtonComponent>
+            </a>
+            </div>
+          </div>
+          <img src="../assets/img/feature-2.png" class="md:w-1/2 m-4 rounded-xl md:rounded-l-xl" alt="Description of the Image">
+        </div>
+      </div>
+      <div class="flex justify-between items-center gap-2 text-content-secondary pt-20 pb-2 px-2 md:px-20 lg:px-40 font-medium text-xs md:text-sm">
+        <div class="hidden md:block">Ocean Starter Kit</div>
+        <div class="">Ocean Starter Kit by <a href="https://github.com/Ron-Digital/ocean-starter-kit" class="text-content-primary font-semibold">Ron Digital</a>. All rights reserved.</div>
+      </div>
+    </footer>
+  </main>
 </template>
