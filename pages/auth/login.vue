922b8f83638cf70a5653319c01f329ac638c266b
ecem <118804065+EcemAydn@users.noreply.github.com>
2023-08-11 18:50:30 +0300

    Update login.vue

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -30,7 +30,7 @@ async function onSubmit() {
     isLoading.value = true;
     await authStore.login(emailInput.value, passwordInput.value, rememberMe.value);
     isLoading.value = false;
-    router.push('/');
+    router.push('/home');
   } catch (err) {
     if (err.errors) {
       const { errors } = err;
@@ -56,76 +56,59 @@ async function onSubmit() {
 }
 </script>
 <template>
-  <div class="flex min-h-full w-full">
-    <div class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-60">
-      <InlineBanner v-if="error" type="negative" title="Error" :description="error"></InlineBanner>
-      <div class="mx-auto w-full max-w-sm lg:w-96">
+
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-10 w-auto" :src="themeConfig.logo" :alt="themeConfig.logoText">
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">{{ t('auth.Sign in to your account') }}</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" @submit.prevent="onSubmit">
         <div>
-          <img class="mx-auto h-12 w-auto mb-12" :src="themeConfig.logo" :alt="themeConfig.logoText" />
-          <h2 class="mt-8 text-3xl leading-9 tracking-wide text-content-primary">
-            {{ t('auth.LoginHeader') }}
-          </h2>
-          <p class="mt-2 text-sm leading-6 text-content-secondary">
-            {{ t('auth.HasNotAccount') }}
-            <RouterLink to="/auth/register" class="font-semibold text-global-blue-500 hover:text-global-blue-600">Get
-              {{ t('auth.Started') }}</RouterLink>
-          </p>
+          <InputComponent
+            :disabled="isLoading"
+            v-model="emailInput"
+            id="email"
+            name="email"
+            type="email"
+            label="E-mail"
+            autocomplete="email"
+            required
+            :error="emailError"
+            @focus="emailError = ''"
+          />
         </div>
 
-        <div class="mt-6">
-          <div>
-            <form @submit.prevent="onSubmit" class="space-y-6">
-              <div>
-                <InputComponent
-                  :disabled="isLoading"
-                  v-model="emailInput"
-                  id="email"
-                  name="email"
-                  type="email"
-                  label="E-mail"
-                  autocomplete="email"
-                  required
-                  :error="emailError"
-                  @focus="emailError = ''"
-                />
-              </div>
-
-              <div>
-                <InputComponent :disabled="isLoading" v-model="passwordInput" id="password" name="password"
-                  type="password" label="Password" autocomplete="current-password" required :error="passwordError"
-                  @focus="passwordError = ''" />
-              </div>
-
-              <div class="flex items-center justify-between">
-                <div class="flex items-center">
-                  <CheckboxComponent :disabled="isLoading" class="cursor-pointer" name="remember-me" type="checkbox"
-                    :label="t('Remember Me')" v-model="rememberMe" />
-                </div>
-                <div class="text-sm leading-6">
-                  <RouterLink to="/auth/forgotpassword">
-                    <ButtonComponent variant="bare" size="small">
-                      Forgot password?
-                    </ButtonComponent>
-                  </RouterLink>
-                </div>
-                <div class="text-sm leading-6">
-                </div>
-              </div>
-
-              <div>
-                <ButtonComponent type="submit" :loading="isLoading" block> {{ t('auth.LoginHeader') }} </ButtonComponent>
-              </div>
-            </form>
+        <div>
+          <InputComponent :disabled="isLoading" v-model="passwordInput" id="password" name="password"
+            type="password" label="Password" autocomplete="current-password" required :error="passwordError"
+            @focus="passwordError = ''" />
+          <div class="flex items-center justify-between">
+            <div class="flex items-center">
+              <CheckboxComponent :disabled="isLoading" class="cursor-pointer" name="remember-me" type="checkbox"
+                :label="t('Remember Me')" v-model="rememberMe" />
+            </div>
+            <div class="text-sm leading-6">
+              <NuxtLink to="/auth/forgotpassword">
+                <ButtonComponent variant="bare" size="small">
+                  Forgot password?
+                </ButtonComponent>
+              </NuxtLink >
+            </div>
           </div>
         </div>
+
+
+      <div>
+        <ButtonComponent type="submit" :loading="isLoading" block> {{ t('auth.LoginHeader') }} </ButtonComponent>
       </div>
-    </div>
-        <div class="relative hidden w-0 flex-1 lg:block">
-      <img
-        class="absolute inset-0 h-full w-full object-cover"
-        src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-        alt=""
-      />
+      </form>
+
+      <p class="mt-10 text-sm leading-6 text-content-secondary text-center">
+        {{ t('auth.HasNotAccount') }}
+        <NuxtLink to="/auth/register" class="font-semibold text-global-blue-500 hover:text-global-blue-600">{{ t('auth.Started') }}</NuxtLink>
+      </p>
     </div>
   </div>
 </template>
